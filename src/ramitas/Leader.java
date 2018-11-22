b55f3b1bac2b8d651ab9827082e5eeeb610e94ed
socamporomani <socamporomani@nautilus10.danielcastelao.org>
2018-11-22 09:00:09 +0100

    clase nueva y modificacion

---
+++ src/ramitas/Leader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ramitas;
+
+/**
+ *
+ * @author socamporomani
+ */
+public class Leader {
+
+    public Leader() {
+        System.out.println("Esta es mi rama pero no la master");
+    }
+    
+}
